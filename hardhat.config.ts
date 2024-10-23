2306d66766c3c5392c8fde4f636a0b0b1a452ed6
Akshat-Bharara <akshat.bharara05@gmail.com>
2024-10-23 15:51:43 +0530

    week 4 task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,22 @@
 import("@nomiclabs/hardhat-ethers");
 import("@nomiclabs/hardhat-waffle");
-import dotenv from "dotenv";
 
 const argv = JSON.parse(env("npm_config_argv"));
-if (argv.original !== ["hardhat", "test"]) {
+if (JSON.stringify(argv.original) !== JSON.stringify(["hardhat", "test"])) {
   require('dotenv').config();
 }
 
 import("./tasks/nft");
 
 import { HardhatUserConfig } from "hardhat/config";
+import { env } from "./lib/env";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.0",
   networks: {
     sepolia: {
       url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.ETH_PRIVATE_KEY],
+      accounts: process.env.ETH_PRIVATE_KEY ? [process.env.ETH_PRIVATE_KEY] : [],
     },
   },
 };
